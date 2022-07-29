9f76fb574a2d6391c3b1d20df4d09d456da95a37
Romulo <romulosilva001@gmail.com>
2022-07-29 14:43:01 -0300

    Criado o primeiro componente first-comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstCompComponent } from './components/first-comp/first-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstCompComponent
   ],
   imports: [
     BrowserModule,
